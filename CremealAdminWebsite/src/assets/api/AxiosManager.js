c1051c1d9dcaa3b51b8d9d9dbfe528b283b1a5fe
Csgroup17 <kh7292000@gmail.com>
2024-08-13 13:47:33 +0300

    Update AxiosManager.js

--- CremealAdminWebsite/src/assets/api/AxiosManager.js
+++ CremealAdminWebsite/src/assets/api/AxiosManager.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-export let apiUrl = 'http://localhost:5241';
+// export let apiUrl = 'http://localhost:5241';
 
-// export let apiUrl = 'https://proj.ruppin.ac.il/cgroup37/test2/tar1';
+export let apiUrl = 'https://proj.ruppin.ac.il/cgroup37/test2/tar1';
 
 const ApiManager = axios.create(
   {
